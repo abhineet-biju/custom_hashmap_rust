0221b6ed1eacfa3b3f2df7c3efc344b969b84c4a
Abhineet Biju <abhi@abhineetbiju.com>
2026-05-27 02:45:56 +0530

    chore: add example main.rs implementation

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use custom_hashmap::hashmap::HashMap;
+fn main() {
+    let mut map = HashMap::<i32, i32>::new();
+    for i in 0..100 {
+        map.insert(i, i);
+    }
+
+    for i in 0..100 {
+        println!("{}", map.get(&i).unwrap());
+    }
+}
