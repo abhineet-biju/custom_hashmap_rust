4aae17e1af95448c5f11615ef884ad3d7f7bcf79
Abhineet Biju <abhi@abhineetbiju.com>
2026-05-26 23:47:23 +0530

    feat: add new() and with_capacity() functions

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -1,2 +1,22 @@
 //! This is a custom hasmap implementation that uses side chaining
 //! using vectors
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+pub struct HashMap<K, V> {
+    buckets: Vec<Vec<(K, V)>>,
+    len: usize,
+}
+
+impl<K: Hash + Eq, V> HashMap<K, V> {
+    pub fn new() -> Self {
+        Self::with_capacity(16)
+    }
+
+    /// Initializes HashMap with given capacity
+    pub fn with_capacity(capacity: usize) -> Self {
+        let buckets = (0..capacity).map(|_| Vec::new()).collect();
+        Self { buckets, len: 0 }
+    }
+}
